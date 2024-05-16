3913cd9b10e3daf76cbf0092d19fef83dd5cbf53
Лобанцов Олексій <lobantsov.oleksii@gmail.com>
2024-05-16 16:15:16 +0300

    fix

--- ConsoleApplication1/ConsoleApplication1/Interpritator/Polish_Inverse_Writing.cpp
+++ ConsoleApplication1/ConsoleApplication1/Interpritator/Polish_Inverse_Writing.cpp
@@ -31,33 +31,36 @@ void Polish_Inverse_Writing::FormingSourceLine()
         }
         else if (isOperation(lex.value))
         {
-            if(lex.value == "write")
-            {
-                Lex write;
-                write.value="Command_write";
-                write.lexID=100;
-                stack_tmp.push(write);
-            }
-            else if(lex.value == "read")
-            {
-                Lex write;
-                write.value="Command_read";
-                write.lexID=101;
-                stack_tmp.push(write);
-            }
-            else if(lex.value =="for" or lex.value=="while" or lex.value=="do")
+            // if(lex.value == "write")
+            // {
+            //     Lex write;
+            //     write.value="Command_write";
+            //     write.lexID=100;
+            //     stack_tmp.push(write);
+            //     //callBack.push(write.value);
+            // }
+            // else if(lex.value == "read")
+            // {
+            //     Lex read;
+            //     read.value="Command_read";
+            //     read.lexID=101;
+            //     stack_tmp.push(read);
+            //     //callBack.push(read.value);
+            // }
+            // if(lex.value == "while" and callBack.top() == "do")
+            // {
+            //     
+            // }
+            if(lex.value =="for" or lex.value=="while" or lex.value=="do")
             {
                 callBack.push(lex.value);
             }
-            else
-            {
                 while (!stack_tmp.empty() && getPriority(stack_tmp.top().value) >= getPriority(lex.value))
                 {
                     source_string_stack.push_back(stack_tmp.top());
                     stack_tmp.pop();
                 }
                 stack_tmp.push(lex);
-            }
         }
         else if (lex.value == "(" || lex.value == "{")
         {
@@ -71,7 +74,14 @@ void Polish_Inverse_Writing::FormingSourceLine()
                 stack_tmp.pop();
             }
             if (!stack_tmp.empty())
+            {
                 stack_tmp.pop();
+                if(stack_tmp.top().value == "write" or stack_tmp.top().value == "read")
+                {
+                    source_string_stack.push_back(stack_tmp.top());
+                    stack_tmp.pop();
+                }
+            }
             if(!stack_tmp.empty() and (callBack.top() == "while" or callBack.top() == "do" or callBack.top() == "if"))
             {
                 Lex command;
@@ -84,6 +94,10 @@ void Polish_Inverse_Writing::FormingSourceLine()
         {
             if (!stack_tmp.empty())
             {
+                if(stack_tmp.top().value == "write" or stack_tmp.top().value == "read")
+                {
+                    stack_tmp.pop();
+                }
                 source_string_stack.push_back(stack_tmp.top());
                 if(!callBack.empty() and callBack.top() == "for")
                 {
@@ -113,6 +127,8 @@ int Polish_Inverse_Writing::getPriority(const string& op)
 {
     if (op == "=") return 1;
     if (op == "+" || op == "-") return 2;
+    if (op == "++" || op == "--" || op == "**") return 2;
+    if (op == "write" || op == "read") return 2;
     if (op == "*" || op == "/") return 3;
     if (op == "<" || op == ">" || op == "<=" || op == ">=" || op == "==") return 4;
     if (op == "(" || op == "{" || op == ")" || op == "}") return -1;
